909c163952e4ba61b966344125faf8aa39ce2b1a
ageng subagja <agengsubagja001@gmail.com>
2021-09-09 12:06:04 +0700

    cek

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:magang/Project-Bale';
+$config['base_url'] = 'http://localhost/balesupply';
 
 
 /*
