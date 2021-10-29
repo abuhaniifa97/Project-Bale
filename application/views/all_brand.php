f19b256a716c93f76d978c0e2dd121a4db7c410e
faishol17 <faishol12permana@gmail.com>
2021-10-29 13:01:45 +0700

    commit

--- application/views/all_brand.php
+++ application/views/all_brand.php
@@ -7,29 +7,38 @@
     <!-- akhir navbar -->
     <!-- Section Slide -->
         <section class=""  >
-                <div class="container" style="max-width: 1367px;">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="border-radius: 20px;">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                            <span class="responsive" ><img src="<?php echo base_url('assets/img/banner53.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="height:auto; border-radius:20px;"></span>
-                            </div>
-                            <div class="carousel-item">
-                            <span class="responsive" ><img src="<?php echo base_url('assets/img/banner_slide.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="height:auto; border-radius:20px;"></span>
-                            </div>
-                            <div class="carousel-item">
-                            <span class="responsive" ><img src="<?php echo base_url('assets/img/banner53.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="height:auto; border-radius:20px;"></span>
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
+		<div class="container" style="max-width: 1367px;">
+				<div id="=carouselExampleControls" class="carousel slide" data-ride="carousel" style="border-radius: 20px;">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+						<span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner1.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border-radius: 20px;"></span>
+						</div>
+						<div class="carousel-item">
+						  <span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner2.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border-radius: 20px;"></span>
+						</div>
+						<div class="carousel-item">
+						 <span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner3.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border radius: 20px; border-radius: 20px;"></span>
+						</div>
+						<div class="carousel-item">
+						 <span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner4.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border radius: 20px; border-radius: 20px;"></span>
+						</div>
+						<div class="carousel-item">
+						 <span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner5.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border radius: 20px; border-radius: 20px;"></span>
+						</div>
+						<div class="carousel-item">
+						 <span class="responsive" ><img src="<?php echo base_url('assets/buatan/banner6.jpg') ?>" class="d-block w-100 img-fluid"  alt="slider 1"  class="rounded" style="width: 100%; height:auto; border radius: 20px; border-radius: 20px;"></span>
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+		</div>
         </section>
     <!-- Akhir Section slide -->
     <!-- awal section kategori -->
@@ -45,50 +54,86 @@
                 </div>
 			</div>
             <div class="container" style="max-width: 1340px;">
-              <div class="row">
-              <div class="col-md-12">
-							<div class="wow" style="border-radius: 30px; box-shadow: 0 5px 10px rgb(73 84 100 / 5%); border-color: transparent; height: width: 100%; 263px;">
+                    <div class="row">
+						<div class="col-md-12">
+							<div class="wow" style="border-radius: 30px; box-shadow: 0 5px 10px rgb(73 84 100 / 5%); border-color: transparent; height: auto">
 								<div class="row">
-                                    <div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+								<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/3second.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/adhdc.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/adidas.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/ardiles.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/arei.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/eiger.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/grenlight.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/arai.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/ikea.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/levis.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2"10
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/mizuno.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/nb.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 									<div class ="col-4 col-md-2 mt-2">
-										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/img/Erigo 2.png') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/nike.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/ortus.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/puma.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/specs.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/ua.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/vans.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/ventela.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/zoya.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/asus.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/acer.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/lenevo.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
+									</div>
+									<div class ="col-4 col-md-2 mt-2">
+										<a href="<?php echo base_url('detail_brand'); ?>"><img src="<?php echo base_url('assets/buatan/garnier.webp') ?>"class="card-img-top img-responsive " style="width: 100%; height: auto;" alt=""></a>
 									</div>
 								</div>
 							</div>
-				</div>
-              </div>
+						</div>
+					</div>	
             </div>
 		</section>
 	<!-- akhir section kategori -->
